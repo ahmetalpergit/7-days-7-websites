2a2f53a3175e17d5283907b51ba494726c5c44f2
Ahmet <ahmetalperwork@gmail.com>
2021-03-11 23:04:35 +0300

    finish all styling and top nav animations

--- bensen/index.js
+++ bensen/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const nav = document.querySelector('.nav');
+const logo = document.getElementById('logo');
+const handleScroll = () => {
+    if (window.pageYOffset > 50) {
+        nav.classList.add('nav-BG');
+        logo.classList.remove('logo--white')
+        logo.classList.add('logo--black')
+    } else {
+        nav.classList.remove('nav-BG');
+        logo.classList.remove('logo--black')
+        logo.classList.add('logo--white')
+    }
+}
+window.addEventListener('scroll', handleScroll);
